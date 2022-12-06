de4ba319f3a3de66f08da4ae21f5ec67918020df
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2022-12-06 15:16:44 +0600

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,19 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
 
-puts "#{values.sample} of #{suits.sample}"
+cards = []
+
+values.each do |value|
+  suits do |suit|
+    cards << "#{value} of #{suits}"
+  end
+end
+
+cards.shuffle!
+
+puts 'How many cards do you pick?'
+number = $stdin.gets.to_i
+
+number.times do
+  puts cards.pop
+end
